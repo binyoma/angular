84aa1ea153d0f90e98b98732082e21a20d468ded
binyoma <innocentbinyoma@pm.me>
2022-04-06 15:14:37 +0200

    lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'angular-video-series';
+   intervalSub
+  ngOnInit() {
+      this.intervalSub = setInterval( () => {
+        console.log('Hello  from ngOnInit');
+      }, 1000);
+  }
+  ngOnDestroy(){
+      if(this.intervalSub){
+       clearInterval(this.intervalSub) 
+      }
+  }
 }
